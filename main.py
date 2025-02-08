91efd37e4e13fb923619faa96578bb966932da68
yell0wrat <superpikachubro@gmail.com>
2025-02-08 13:56:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ elif choice == 2:
 elif choice == 3:
     print('you have picked scissors')
 else:
-    print('you did not choose a number 1-3, fuck you.')
+    print('you did not choose a number 1-3, please pick 1-3 next time! : (')
+    while choice < 1 or choice > 3:
+        choice = int(input('pick a number 1-3, rock is 1, paper is 2, scissors is 3: '))
+if choice == 1:
+    print('you have picked rock')
+elif choice == 2:
+    print('you have picked paper')
+elif choice == 3:
+    print('you have picked scissors')
 print('the computer is deciding what to pick')
 computer=random.randint(4,6)
 if computer == 4:
